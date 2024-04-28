503cc068fb8454aabe9a43add099b6d1ca28c3f4
brielross <2011jet1@gmail.com>
2024-04-27 23:45:11 -0600

    Added teapot and lighting

---
+++ src/drawable/teapot.rs
@@ -0,0 +1,85 @@
+use std::fs;
+
+use glium::{glutin::surface::WindowSurface, DrawParameters, Surface, VertexBuffer};
+
+mod teapot_data;
+
+pub struct Teapot {
+    positions: VertexBuffer<teapot_data::Vertex>,
+    normals: VertexBuffer<teapot_data::Normal>,
+    indices: glium::IndexBuffer<u16>,
+    program: glium::Program,
+}
+
+impl Teapot {
+    pub fn new(display: glium::backend::glutin::Display<WindowSurface>) -> Teapot {
+        let positions = glium::VertexBuffer::new(&display, &teapot_data::VERTICES).unwrap();
+        let normals = glium::VertexBuffer::new(&display, &teapot_data::NORMALS).unwrap();
+        let indices = glium::IndexBuffer::new(
+            &display,
+            glium::index::PrimitiveType::TrianglesList,
+            &teapot_data::INDICES,
+        )
+        .unwrap();
+
+        let use_gourad = false;
+
+        let v_gourad_src = "../shaders/gourad_vertex.glsl";
+        let v_blinn_phong_src = "../shaders/blinn_phong_vertex.glsl";
+        let vertex_shader_file = if use_gourad {
+            v_gourad_src
+        } else {
+            v_blinn_phong_src
+        };
+        let vertex_shader_src = fs::read_to_string(vertex_shader_file).unwrap();
+        let vertex_shader_src = vertex_shader_src.as_str();
+
+        let f_gourad_src = "../shaders/gourad_fragment.glsl";
+        let f_blinn_phong_src = "../shaders/blinn_phong_fragment.glsl";
+        let fragment_shader_file = if use_gourad {
+            f_gourad_src
+        } else {
+            f_blinn_phong_src
+        };
+        let fragment_shader_src = fs::read_to_string(fragment_shader_file).unwrap();
+        let fragment_shader_src = fragment_shader_src.as_str();
+
+        let program =
+            glium::Program::from_source(&display, vertex_shader_src, fragment_shader_src, None)
+                .unwrap();
+
+        return Teapot {
+            positions,
+            normals,
+            indices,
+            program,
+        };
+    }
+
+    pub fn draw_teapot(
+        &self,
+        target: &mut glium::Frame,
+        params: DrawParameters,
+        perspective: [[f32; 4]; 4],
+        view: [[f32; 4]; 4],
+    ) {
+        let model = [
+            [0.01, 0.0, 0.0, 0.0],
+            [0.0, 0.01, 0.0, 0.0],
+            [0.0, 0.0, 0.01, 0.0],
+            [0.0, 0.0, 0.0, 1.0f32],
+        ];
+
+        let light = [-2.5, 0.4, 0.9f32];
+
+        target
+            .draw(
+                (&self.positions, &self.normals),
+                &self.indices,
+                &self.program,
+                &uniform! { model: model, view: view, u_light: light, perspective: perspective },
+                &params,
+            )
+            .unwrap();
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 use glium::Surface;
 use std::time::Instant;
 
-mod drawables;
+#[path = "drawable/teapot.rs"]
+mod teapot;
 
 #[macro_use]
 extern crate glium;
@@ -12,10 +13,12 @@ fn main() {
         .expect("event loop building");
     let (window, display) = glium::backend::glutin::SimpleWindowBuilder::new().build(&event_loop);
 
-    let mut t: f32 = 0.0;
-
     let start_time = Instant::now();
     let mut frames = 0;
+
+    let teapot = teapot::Teapot::new(display.clone());
+
+    let mut t = 0f32;
 
     let _ = event_loop.run(move |event, window_target| {
         match event {
@@ -28,9 +31,25 @@ fn main() {
                 winit::event::WindowEvent::RedrawRequested => {
                     t += 0.02;
                     let mut target = display.draw();
-                    target.clear_color(0.0, 0.0, 1.0, 1.0);
-                    drawables::draw_image(display.clone(), &mut target);
-                    drawables::draw_triangle(display.clone(), &mut target, t);
+                    target.clear_color_and_depth((0.0, 0.0, 1.0, 1.0), 1.0);
+                    let params = glium::DrawParameters {
+                        depth: glium::Depth {
+                            test: glium::draw_parameters::DepthTest::IfLess,
+                            write: true,
+                            ..Default::default()
+                        },
+                        // Enables backface culling
+                        //backface_culling:
+                        //    glium::draw_parameters::BackfaceCullingMode::CullClockwise,
+                        ..Default::default()
+                    };
+                    let perspective = get_perspective_matrix(&window);
+                    let view = get_view_matrix(
+                        &[3.0 * t.cos(), 1.5 * t.cos(), 3.0 * t.sin()],
+                        &[-1.0 * t.cos(), -0.5 * t.cos(), -1.0 * t.sin()],
+                        &[0.0, 1.0, 0.0],
+                    );
+                    teapot.draw_teapot(&mut target, params, perspective, view);
                     target.finish().unwrap();
                     frames += 1;
                 }
@@ -42,4 +61,68 @@ fn main() {
             _ => (),
         };
     });
+}
+
+fn get_perspective_matrix(window: &winit::window::Window) -> [[f32; 4]; 4] {
+    let perspective = {
+        let window_size = window.inner_size();
+        let width = window_size.width;
+        let height = window_size.height;
+        let aspect_ratio = height as f32 / width as f32;
+
+        let fov: f32 = 3.141592 / 3.0;
+        // Max and min depth values
+        let zfar = 1024.0;
+        let znear = 0.1;
+
+        let f = 1.0 / (fov / 2.0).tan();
+
+        [
+            [f * aspect_ratio, 0.0, 0.0, 0.0],
+            [0.0, f, 0.0, 0.0],
+            [0.0, 0.0, (zfar + znear) / (zfar - znear), 1.0],
+            [0.0, 0.0, -(2.0 * zfar * znear) / (zfar - znear), 0.0],
+        ]
+    };
+    return perspective;
+}
+
+fn get_view_matrix(position: &[f32; 3], direction: &[f32; 3], up: &[f32; 3]) -> [[f32; 4]; 4] {
+    let f = {
+        let f = direction;
+        let len = f[0] * f[0] + f[1] * f[1] + f[2] * f[2];
+        let len = len.sqrt();
+        [f[0] / len, f[1] / len, f[2] / len]
+    };
+
+    let s = [
+        up[1] * f[2] - up[2] * f[1],
+        up[2] * f[0] - up[0] * f[2],
+        up[0] * f[1] - up[1] * f[0],
+    ];
+
+    let s_norm = {
+        let len = s[0] * s[0] + s[1] * s[1] + s[2] * s[2];
+        let len = len.sqrt();
+        [s[0] / len, s[1] / len, s[2] / len]
+    };
+
+    let u = [
+        f[1] * s_norm[2] - f[2] * s_norm[1],
+        f[2] * s_norm[0] - f[0] * s_norm[2],
+        f[0] * s_norm[1] - f[1] * s_norm[0],
+    ];
+
+    let p = [
+        -position[0] * s_norm[0] - position[1] * s_norm[1] - position[2] * s_norm[2],
+        -position[0] * u[0] - position[1] * u[1] - position[2] * u[2],
+        -position[0] * f[0] - position[1] * f[1] - position[2] * f[2],
+    ];
+
+    [
+        [s_norm[0], u[0], f[0], 0.0],
+        [s_norm[1], u[1], f[1], 0.0],
+        [s_norm[2], u[2], f[2], 0.0],
+        [p[0], p[1], p[2], 1.0],
+    ]
 }
